7368ba16a4c3801fda21985131f3f0c17cad5a49
ZsoltTirkala <tirkala.zsolt@gmail.com>
2020-04-06 16:01:57 +0200

    Updated requirements.txt and data_manager_sql.py test

--- data_manager_sql.py
+++ data_manager_sql.py
@@ -2,3 +2,15 @@ from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
 import database_common_sql
 
+
+@database_common_sql.connection_handler
+def get_boards(cursor: RealDictCursor) -> list:
+    query = """
+            SELECT *
+            FROM boards
+            ORDER BY id
+            """
+
+    cursor.execute(query)
+
+    return cursor.fetchall()
